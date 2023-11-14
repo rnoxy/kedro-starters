93649306ab9204cdacc2f5316c89e286ca885afc
Rafał Nowak <rafal.nowak@tooploox.com>
2023-11-14 22:45:48 +0100

    empty pipeline

---
+++ mini-poetry/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipelines/data_processing/pipeline.py
@@ -0,0 +1,18 @@
+from kedro.pipeline import Pipeline, pipeline, node
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=lambda x: x,
+                inputs="raw_data",
+                outputs="processed_data",
+                name="data_processing",
+            )
+        ],
+        inputs=None,
+        outputs=None,
+        parameters=None,
+        namespace=None,
+        tags=None,
+    )
